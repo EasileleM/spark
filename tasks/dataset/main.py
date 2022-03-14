63bffed142a8b8284d42ddb069710757398b4e41
Savva Dzhumaliev <savva.dzhumaliev@sbermarket.ru>
2022-03-14 22:40:00 +0400

    lol

---
+++ tasks/dataset/main.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import mean
+
+student_data_filename = "/tasks/bruteforce/student_data.csv"
+spark = SparkSession.builder.appName("Test").master("spark://172.24.0.8:7077").getOrCreate()
+
+student_data = spark.read \
+    .option("header", True) \
+    .csv(student_data_filename) \
+    .select("medu", "fedu", "g3") \
+    .repartition(6) \
+    .groupBy("medu", "fedu") \
+    .agg(mean('g3')) \
+    .repartition(1) \
+    .write \
+    .option("header", "true") \
+    .csv("/tasks/brutef               orce/result")
+
+spark.stop()
